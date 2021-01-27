0f0f6dec5a44e0ad8a8da9fe4d27f861f4321cb9
Clement Walter <clement0walter@gmail.com>
2021-01-27 15:53:31 +0100

    Silent warning in test

--- tests/addons/map_test.py
+++ tests/addons/map_test.py
@@ -8,10 +8,10 @@ from pandas_addons.addons.map import map
 class TestMap:
     @patch.object(pd.Series, "map")
     def test_should_call_base_map(self, mock_map):
-        map(pd.Series(), sentinel.args)
+        map(pd.Series(dtype="object"), sentinel.args)
         mock_map.assert_called_once_with(sentinel.args)
 
     @patch("multiprocessing.pool.Pool.imap")
     def test_should_call_imap(self, mock_imap):
-        map(pd.Series(), sentinel.args, parallel=True)
+        map(pd.Series(dtype="object"), sentinel.args, parallel=True)
         mock_imap.assert_called_once_with(sentinel.args, [])
